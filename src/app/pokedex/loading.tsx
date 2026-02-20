87b522d5309bc2c6bdf9b7a3b04de17a5a99fd0f
CoffeePuma55644 <coffeepuma55644@gmail.com>
2026-02-20 11:49:50 +0100

    Update loading.tsx
    forgot className instead of class in React

--- src/app/pokedex/loading.tsx
+++ src/app/pokedex/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div class="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-bars loading-xl"></span>
     </div>
   );
